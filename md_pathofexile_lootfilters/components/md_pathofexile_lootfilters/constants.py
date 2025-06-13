d182abd72fb90db812dd1c7c6c13b1932a2889a9
Matthew van der Hoorn <md.registrations@protonmail.com>
2025-06-14 00:55:02 +0200

    Added build test filter configuration toggle

--- md_pathofexile_lootfilters/components/md_pathofexile_lootfilters/constants.py
+++ md_pathofexile_lootfilters/components/md_pathofexile_lootfilters/constants.py
@@ -4,21 +4,25 @@ from typing import List
 from md_pathofexile_lootfilters.components.md_pathofexile_lootfilters.config.class_lookup import BuildType, \
     get_associated_weapons, WeaponTypeClass, get_unassociated_weapons
 
+# ---- Configurable ----
 APP_NAME: str = "MD.PathOfExile.LootFilters"
 MAX_SEPARATOR_LENGTH: int = 80
 
 DEBUG_MODE: bool = False
 VERBOSE: bool = False
 
-ROOT: Path = Path(__file__).parent.parent.parent.parent
-CONFIG_DIR: Path =  ROOT / "md_pathofexile_lootfilters" / "components" / "md_pathofexile_lootfilters" / "config"
+BUILD_TEST_FILTER: bool = True
 
 OUTPUT_DIRECTORIES: List[Path] = [
     Path(r"X:\MD.PathOfExile.LootFilters\output"),
     Path(r"C:\Users\LordMartron\Documents\My Games\Path of Exile")
 ]
 
-FILTER_NAME: str = "MD.TestFilter.filter"
+# ---- Automatic ----
+ROOT: Path = Path(__file__).parent.parent.parent.parent
+CONFIG_DIR: Path =  ROOT / "md_pathofexile_lootfilters" / "components" / "md_pathofexile_lootfilters" / "config"
+
+FILTER_NAME: str = "MD.TestFilter.filter" if BUILD_TEST_FILTER else "MD.SSF-SC&HC-Filter.filter"
 
 SELECTED_BUILD_TYPE: BuildType = BuildType.MeleeSpellcaster
 
